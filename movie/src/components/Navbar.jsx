5dd15360427c150d730a19d16912cc8579ab00a5
Aakash Soni <soniaakash5336@gmail.com>
2025-01-10 15:09:10 +0530

    update

--- movie/src/components/Navbar.jsx
+++ movie/src/components/Navbar.jsx
@@ -1,35 +1,42 @@
-import React,{useState} from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './style.css'; // Importing the CSS file
 
 const Navbar = () => {
-  const [query, setQuery] = useState("")
-  const navigate = useNavigate()
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
   const handleSearch = (e) => {
     e.preventDefault();
-    if (query){
-      navigate(`/search?query=${query}`)
+    if (query) {
+      navigate(`/search?query=${query}`);
     }
-  }
+  };
+
   return (
-    <nav>
-      <h1>Movie Search App</h1>
-      <form  onSubmit={handleSearch}>
-        <input type="text"  
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder='Search Movie...'/>
-        <button type='submit'>Search</button>
-      </form>
-      <div className='links'>
-        <a href="/"> Popular</a>
-        <a href="/top-rated"> Top-Rated</a>
-        <a href="/upcoming-Page"> Upcoming</a>
+    <nav className="navbar ">
+      <div className="navbar-container">
+        <h1 className="navbar-logo">Movie Search App</h1>
+        <form className="navbar-search" onSubmit={handleSearch}>
+          <input
+            type="text"
+            className="navbar-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search Movie..."
+          />
+          <button type="submit" className="navbar-button">Search</button>
+        </form>
+        <div className="navbar-links">
+          <a href="/" className="navbar-link">Popular</a>
+          <a href="/top-rated" className="navbar-link">Top-Rated</a>
+          <a href="/upcoming-Page" className="navbar-link">Upcoming</a>
+        </div>
       </div>
     </nav>
-  )
     
-  
-}
+  );
+};
 
-export default Navbar
+
+export default Navbar;
--- movie/src/pages/SearchedMoviePage.jsx
+++ movie/src/pages/SearchedMoviePage.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { useLocation } from 'react-router-dom';  //gives current url/path
+import Moviecard from '../components/MovieCard';
+
+
+
+const API_KEY = 'c45a857c193f6302f2b5061c3b85e743'; 
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+//&language=en-US&page=1 => Query Paramenter
+// <int:id> => Body parameter
+
+function usequery(){
+  return new URLSearchParams(useLocation().search)
+}
+
 
 const SearchedMoviePage = () => {
+
+  const [movies, setMovies] = useState([])
+
+  const [loading, setLoading] = useState(true)
+
+  const query = usequery().get('query');  // search?query=Wakeupsid
+  
+
+
+  
   return (
     <div>SearchedMoviePage</div>
   )
